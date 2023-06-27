7340480c042603383a143337bfb827b3f3b18d74
Ryo616 <icaras84@gmail.com>
2023-06-26 21:19:43 -0700

    pose editor ui slight change

--- src/com/icaras84/rrcodegenerator/core/utils/extraui/Pose2dJPanel.java
+++ src/com/icaras84/rrcodegenerator/core/utils/extraui/Pose2dJPanel.java
@@ -10,30 +10,35 @@ import java.text.NumberFormat;
 
 public class Pose2dJPanel extends JPanel{
 
-    public static final int WIDTH = 200, HEIGHT = 90;
+    public static final int WIDTH = 150, HEIGHT = 90;
 
     private static final NumberFormatter numFormat;
 
     static { //set it up no matter what
-        NumberFormat realNumFormat = NumberFormat.getIntegerInstance();
-        realNumFormat.setMaximumFractionDigits(4);
+        NumberFormat realNumFormat = NumberFormat.getNumberInstance();
         numFormat = new NumberFormatter(realNumFormat);
         numFormat.setValueClass(Double.class);
-        numFormat.setAllowsInvalid(false); //say 'no' to letters
+        numFormat.setAllowsInvalid(true); //say 'no' to letters
         numFormat.setCommitsOnValidEdit(true);
     }
 
     private JFormattedTextField xTextBox, yTextBox, headingTextBox;
-    private JLabel xLabel, yLabel, headingLabel;
+    private String editorTitle;
+    private JLabel xLabel, yLabel, headingLabel, editorTitleLabel;
     private double poseX, poseY, poseHeading;
 
     public Pose2dJPanel(){
+        this("Pose2d Editor");
+    }
+
+    public Pose2dJPanel(String editorTitle){
         super();
         setLayout(new GridBagLayout());
         setSize(WIDTH, HEIGHT);
         setMinimumSize(getSize());
         setMaximumSize(getSize());
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        this.editorTitle = editorTitle;
         initUI();
     }
 
@@ -44,15 +49,13 @@ public class Pose2dJPanel extends JPanel{
 
     private void createUI() {
         xTextBox = new JFormattedTextField(numFormat);
-        xTextBox.setSize(175, 100);
         yTextBox = new JFormattedTextField(numFormat);
-        yTextBox.setSize(175, 100);
         headingTextBox = new JFormattedTextField(numFormat);
+
 
         xTextBox.setText("0");
         yTextBox.setText("0");
         headingTextBox.setText("0");
-
 
         xTextBox.addPropertyChangeListener("value", this::retrieveX);
         yTextBox.addPropertyChangeListener("value", this::retrieveY);
@@ -60,7 +63,9 @@ public class Pose2dJPanel extends JPanel{
 
         xLabel = new JLabel("X:");
         yLabel = new JLabel("Y:");
-        headingLabel = new JLabel("Heading (DEG):");
+        headingLabel = new JLabel("θ (DEG):");
+
+        editorTitleLabel = new JLabel(editorTitle);
 
         xLabel.setHorizontalTextPosition(SwingConstants.RIGHT);
         yLabel.setHorizontalTextPosition(SwingConstants.RIGHT);
@@ -69,24 +74,28 @@ public class Pose2dJPanel extends JPanel{
 
     private void arrangeUI(){
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.fill = GridBagConstraints.BOTH;
         constraints.weightx = 1;
         constraints.weighty = 1/3d;
 
+        constraints.gridwidth = 16;
+        add(editorTitleLabel, constraints);
+        constraints.gridwidth = 1;
+
         constraints.gridx = 0; //label positioning
-        constraints.gridy = 0;
-        add(xLabel, constraints);
         constraints.gridy = 1;
-        add(yLabel, constraints);
+        add(xLabel, constraints);
         constraints.gridy = 2;
+        add(yLabel, constraints);
+        constraints.gridy = 3;
         add(headingLabel, constraints);
 
         constraints.gridx = 1; //TextBox positioning
-        constraints.gridy = 0;
-        add(xTextBox, constraints);
+        constraints.gridwidth = 15;
         constraints.gridy = 1;
-        add(yTextBox, constraints);
+        add(xTextBox, constraints);
         constraints.gridy = 2;
+        add(yTextBox, constraints);
+        constraints.gridy = 3;
         add(headingTextBox, constraints);
     }
 
@@ -109,7 +118,6 @@ public class Pose2dJPanel extends JPanel{
         System.out.println(this);
     }
 
-    @Override
     public String toString(){
         return "X: " + poseX + "\nY: " + poseY + "\nHeading (DEG): " + poseHeading;
     }
